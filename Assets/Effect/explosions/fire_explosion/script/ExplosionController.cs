682ccee9ec6bc61f60d34716e6b2179c79c03109
huyhoangse97 <80676594+huyhoangse97@users.noreply.github.com>
2022-01-16 20:20:19 +0700

    Testing Particle Collision Module

---
+++ Assets/Effect/explosions/fire_explosion/script/ExplosionController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+    private Rigidbody2D rgbd2D;
+    [SerializeField]private float speed, duration;
+    [SerializeField] private GameObject player;
+    private Vector3 distanceToPlayer = new Vector3(1.41f, 0.21f, 5f);
+
+    GameObject FindGameObject(string objectName){
+        GameObject result = GameObject.Find(objectName);
+        if (result == null){
+            Debug.LogError("Could not find object '" + objectName + "';");
+            return null;
+        }
+        else return result;
+    }
+
+    public void Awake(){
+        // player = FindGameObject("BabyDragon");
+        transform.position = player.transform.position + distanceToPlayer;
+        Destroy(gameObject, 12f);
+    }
+
+    public void Start(){
+        rgbd2D = GetComponent<Rigidbody2D>();
+        MoveOn();
+    }
+
+    public void MoveOn(){
+        StartCoroutine(Movement());
+    }
+
+    private Vector2 vector2Zero = new Vector2(0, 0);
+    IEnumerator Movement(){
+        rgbd2D.velocity = vector2Zero;
+        yield return new WaitForSeconds(2f);
+        rgbd2D.velocity = new Vector2(speed/2, 0f);
+        yield return new WaitForSeconds(0.5f);
+        rgbd2D.velocity = new Vector2(speed, 0f);
+        yield return new WaitForSeconds(2f);
+        rgbd2D.velocity = new Vector2(speed/2, 0f);
+        yield return new WaitForSeconds(0.5f);
+        rgbd2D.velocity = vector2Zero;
+    }
+}
---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    /**
+     * * Must check: Send Collision Messages in Particle Collision module
+    */
+    private void OnParticleCollision(GameObject other) {
+        if (other.layer == 9){//Layer skill
+            print("Collision skill effect");
+            Rigidbody2D rgbd = other.gameObject.GetComponent<Rigidbody2D>();
+            rgbd.AddForce(Vector2.down);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.layer == 9){//Layer skill
+            print("Collision skill effect");
+            // Rigidbody2D rgbd = other.gameObject.GetComponent<Rigidbody2D>();
+            Rigidbody2D rgbd = gameObject.GetComponent<Rigidbody2D>();
+            rgbd.AddForce(Vector2.down*200);
+            // rgbd.velocity = new Vector2(0, 2);
+        }
+    }
+
+}
